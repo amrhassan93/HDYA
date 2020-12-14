496d160aca05d061c457e4019fb7428835d9faa2
Amr Hassan <amr_hassan522@yahoo.com>
2020-12-14 23:32:25 +0200

    delete backend

--- HdyaBack/product/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from product.models import Product , Category , RelationShip , Occassion , ProductPicture
-# Register your models here.
-admin.site.register(ProductPicture)
-admin.site.register(RelationShip)
-admin.site.register(Occassion)
-admin.site.register(Category)
-admin.site.register(Product)
