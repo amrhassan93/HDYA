91b12fd7595789bb6598e728ed4f883a886e680e
moomenrazik <37555587+moomenrazik@users.noreply.github.com>
2020-12-24 19:00:35 +0200

    About us Page, Pipe dollar to egp and fix design of search and flash div

---
+++ HdyaFront/src/app/Pipes/dollartoegp.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ENGINE_METHOD_PKEY_ASN1_METHS } from 'constants';
+
+@Pipe({
+  name: 'dollartoegp'
+})
+export class DollartoegpPipe implements PipeTransform {
+
+  transform(value:number,changeRate:number=15.70): string {
+    let result = value*changeRate
+    return `${result} EGP`;
+
+  }
+}
