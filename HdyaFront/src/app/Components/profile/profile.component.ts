d5dee7d3f4e0f3914232c8667da038537cda0644
Mohamed Yasser <egyria2011@gmail.com>
2020-12-26 20:38:54 +0200

    edit and profile pages

--- HdyaFront/src/app/Components/profile/profile.component.ts
+++ HdyaFront/src/app/Components/profile/profile.component.ts
@@ -3,7 +3,6 @@ import { AuthenticationService } from '../../services/authentication.service'
 import { ActivatedRoute, Router } from '@angular/router';
 import { Profile } from '../../models/interfaces/profile'
 
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
