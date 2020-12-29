c68da76a9fd4abfb76db18f612df455a581b7668
Amr Hassan <amr_hassan522@yahoo.com>
2020-12-30 00:00:21 +0200

    fix add to cart from product details bug

--- HdyaFront/src/app/services/add-to-cart.service.ts
+++ HdyaFront/src/app/services/add-to-cart.service.ts
@@ -1,84 +1,38 @@
 import { Injectable } from '@angular/core';
 import { ProductsService } from '../services/products.service'
-import { Product } from '../models/interfaces/product'
-import { Category } from '../models/interfaces/category'
 import { AlertService } from 'src/app/_alert';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddToCartService {
-  productList: Product[] = [];
   cart:Array<object> = [];
-  totalRecords: number | undefined
-  categoryList:Category[]=[];
-  moreData:Product[]= []
-  relationships: any;
-  occassions: any;
 
-  constructor(private _products:ProductsService ,protected alertService: AlertService) {
-    if(localStorage.getItem('products')){
-
-      let searchProducts = localStorage.getItem('products')
-      // console.log(searchProducts instanceof Array);
-      
-      this.productList = JSON.parse(searchProducts || '{}')
-      this.totalRecords = this.productList.length
-      localStorage.removeItem('products')
-      
-    }
-    else {
-      this._products.viewProducts().subscribe(
-        (data)=>{
-          this.productList=data.results;
-          this.totalRecords = data.results.length
-          // console.log(this.productList);
-          this.moreData = data
-          // this.allproducts = this.productList
-        },
-        (err)=> console.log(err) 
-      )    
-    }
-
-    this._products.showcategories().subscribe(
-      (data)=>this.categoryList = data.results,
-      (err) => console.log(err) 
-    )
-    this._products.showrelations().subscribe(
-      (data)=>this.relationships=data.results,
-      (err)=>console.log(err) 
-    )
-
-    this._products.showoccassions().subscribe(
-      (data)=>this.occassions=data.results,
-      (err)=>console.log(err) 
-    )
-   }
-
-
+  constructor(private _products:ProductsService ,protected alertService: AlertService) {}
    addCart(product_id:number){
     
-    if (localStorage.getItem("cart")){
-      this.cart = JSON.parse(localStorage.getItem("cart") || '{}') 
+    let addtocart;
 
-      let addtocart = this.productList.find((product)=>{ 
-        return product.id == product_id
-        })
-
-      this.cart.push(addtocart)
-
-      localStorage.setItem("cart" , JSON.stringify(this.cart))
-      this.alertService.success('Success!!', this.options)
-    }
-    else {
-      let addtocart = this.productList.find((product)=>{ 
-        return product.id == product_id
-        })
-      this.cart.push(addtocart)
-      localStorage.setItem("cart" , JSON.stringify(this.cart))
-      // console.log(this.cart);
-      this.alertService.success('Success!!', this.options)
-    }
+    this._products.viewProductById(product_id).subscribe(
+      (data)=>{
+        addtocart = data
+        console.log(addtocart);
+        if (localStorage.getItem("cart")){
+          this.cart = JSON.parse(localStorage.getItem("cart") || '{}') 
+          this.cart.push(addtocart)
+    
+          localStorage.setItem("cart" , JSON.stringify(this.cart))
+          this.alertService.success('Success!!', this.options)
+        }
+        else {
+          this.cart.push(addtocart)
+          localStorage.setItem("cart" , JSON.stringify(this.cart))
+          // console.log(this.cart);
+          this.alertService.success('Success!!', this.options)
+        }
+      },
+      (err)=>console.log(err)
+    )
   }
 
 }
