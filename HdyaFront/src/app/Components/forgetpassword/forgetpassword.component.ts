144e2fe508a00ffaa1c06cd080f3b3cb80262d78
Amr Hassan <amr_hassan522@yahoo.com>
2021-01-04 11:09:21 +0200

    reset password

--- HdyaFront/src/app/Components/forgetpassword/forgetpassword.component.ts
+++ HdyaFront/src/app/Components/forgetpassword/forgetpassword.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service'
+
 
 @Component({
   selector: 'app-forgetpassword',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgetpassword.component.scss']
 })
 export class ForgetpasswordComponent implements OnInit {
-
-  constructor() { }
+  email:string = ""
+  constructor(private _auth:AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  reset(){
+    this._auth.resetPassword(this.email).subscribe(
+      (data)=>{alert("Check Your Email") }, console.log)
   }
 
 }
